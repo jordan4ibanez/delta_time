9472b6d491f9deaff5177d4bac3d4abb1e2a3db9
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-08-16 04:00:22 -0400

    Fix horrible code quality

--- source/delta_time.d
+++ source/delta_time.d
@@ -2,29 +2,29 @@ module delta_time;
 
 import core.time;
 
-public static class Delta {
 
-    // Start with delta of 0
-    private static MonoTime before = MonoTime.zero;
-    private static MonoTime after = MonoTime.zero;
+// Start with delta of 0
+private MonoTime before = MonoTime.zero;
+private MonoTime after = MonoTime.zero;
 
     // High precision delta for users with powerful computers
-    private static double delta = 0;
+private double delta = 0;
 
-    // Calculates the delta time between frames. First will always be 0
-    public static void calculateDelta() {
-        this.after = MonoTime.currTime;
-        Duration duration = after - before;
-        this.delta = cast(double)duration.total!("nsecs") / 1_000_000_000.0;
-        this.before = MonoTime.currTime;
-    }
+// Calculates the delta time between frames. First will always be 0
+void calculateDelta() {
+    after = MonoTime.currTime;
+    Duration duration = after - before;
+    delta = cast(double)duration.total!("nsecs") / 1_000_000_000.0;
 
-    // Allows any function or object to get the delta anywhere in the program
-    public static double getDelta() {
-        return this.delta;
+    // Quick fix for zero start time becoming crazy
+    if (delta > 1000) {
+        delta = 0;
     }
+    
+    before = MonoTime.currTime;
 }
 
-// Users do not have to explicitly call Delta class
-alias getDelta = Delta.getDelta;
-alias calculateDelta = Delta.calculateDelta;
+// Allows any function or object to get the delta anywhere in the program
+double getDelta() {
+    return delta;
+}
